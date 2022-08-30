57ff2ef7022f7b099d18b052134331c3a23c37c0
Karol Harezlak (karolh2000) <karolh@jfrog.com>
2022-08-30 10:38:55 -0700

    Refactoring (package, names)

--- src/main/java/org/example/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class HelloWorld {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!, Simple app to test Pyrsia Maven support!" );
-    }
-}
